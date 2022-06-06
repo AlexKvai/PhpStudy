952eb84eceb772c7017b9ee76fe97cecf8d8109f
alex parfenov <alexp-2004@mail.ru>
2022-06-06 13:37:12 +0700

    Nice to meet you methods of class)

--- OOP/classes/Car.php
+++ OOP/classes/Car.php
@@ -5,5 +5,15 @@ class Car
   public $wheels = 4;
   public $speed = 180;
   public $brand;
+
+  function getCarInfo() {
+    return "<h3>О моём авто:</h3>
+    Марка: {$this->brand} <br>
+    Колёс: {$this->wheels} <br>
+    Цвет: {$this->color} <br>
+    Скорость: {$this->speed} <br>
+    ";
+  }
+
 }
 ?>
--- OOP/index.php
+++ OOP/index.php
@@ -2,17 +2,13 @@
 require_once 'classes/MyFirstClass.php';
 require_once 'classes/Car.php';
 
-echo 111; 
 
 $car = new Car();
 $car->color = 'black';
 $car->brand = 'volvo';
 
-echo "<h3>О моём авто:</h3>
-      Марка: {$car->brand} <br>
-      Колёс: {$car->wheels} <br>
-      
-      ";
+
+echo $car->getCarInfo();
 
 // $FirstObj = new MyFirstClass();
 // var_dump($FirstObj);
